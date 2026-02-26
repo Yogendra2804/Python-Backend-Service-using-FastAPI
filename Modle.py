a33c8b4054334a3623e94a81c949ee9939e32c0b
Yogendra Gupta <yogendra.gupta2023@vitstudent.ac.in>
2026-02-26 15:49:30 +0530

    Add files via upload

---
+++ Modle.py
@@ -0,0 +1,27 @@
+from pydantic import field_validator, BaseModel
+from Telusko_FASTAPI.engine import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
+
+
+class Value(BaseModel):
+    IIN: int
+
+    @field_validator("IIN")
+    @classmethod
+    def check_len(clv, data):
+        if len(str(data)) != 6:
+            return "Length should be exectly 6"
+        return data
+
+
+class CARDS(Base):
+    __tablename__ = "CARDS"
+
+    bin_iin: Mapped[int] = mapped_column("BIN/IIN", Integer, primary_key=True)
+    network_Scheme: Mapped[str] = mapped_column(
+        "Network/Scheme", String, nullable=False
+    )
+    card_type: Mapped[str] = mapped_column("Card Type", String, nullable=False)
+    card_category: Mapped[str] = mapped_column("Card Category", String, nullable=False)
+    issuer: Mapped[str] = mapped_column("Issuer", String, nullable=False)
---
+++ ORM_Find_CARDS.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from Modle import CARDS, Value
+from Telusko_FASTAPI.engine import session
+
+app = FastAPI()
+# -------------------------------------------------------------------------------------------------------
+# ORM 
+
+
+@app.get("/cards")
+def ORM_get_CARD_details(iin: int):
+    card = session.get(CARDS, iin)
+    try:
+        if card:
+            return {
+                "BIN/IIN": card.bin_iin,
+                "Network/Scheme": card.network_Scheme,
+                "Card Type": card.card_type,
+                "Card Category": card.card_category,
+                "Issuer": card.issuer,
+            }
+        session.close()
+    except:
+        session.close()
+        return {"message": "You did not put the correct details. "}
+
+
+@app.put("/cards")
+def ORM_post_get_CARDS(data: Value):
+    card = session.get(CARDS, data.IIN)
+    try:
+        if card:
+            return {
+                "BIN/IIN": card.bin_iin,
+                "Network/Scheme": card.network_Scheme,
+                "Card Type": card.card_type,
+                "Card Category": card.card_category,
+                "Issuer": card.issuer,
+            }
+        session.close()
+    except:
+        session.close()
+        return {"message": "ORM expect caught something."}
